fe81bb683f1707a45e8502408475be551ee840c1
konalla <nalla79bah@gmail.com>
2026-07-07 08:07:11 +0800

    Allow local web fallback CORS origins

--- apps/api/src/http/respond.ts
+++ apps/api/src/http/respond.ts
@@ -4,6 +4,10 @@ import type { ApiErrorResponse } from "@prima-wash/contracts";
 const allowedLocalOrigins = new Set([
   "http://127.0.0.1:3000",
   "http://localhost:3000",
+  "http://127.0.0.1:3020",
+  "http://localhost:3020",
+  "http://127.0.0.1:3021",
+  "http://localhost:3021",
   "http://127.0.0.1:8081",
   "http://localhost:8081",
   "http://127.0.0.1:8082",
